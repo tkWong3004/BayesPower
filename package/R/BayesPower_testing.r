0a61e5b3dfbaf2b6f5b97d6efea4666f9220adca
tkWong3004 <t.k.wong3004@gmail.com>
2025-06-13 14:27:49 +0200

    bug

--- package/R/BayesPower_testing.r
+++ package/R/BayesPower_testing.r
@@ -1,4 +1,33 @@
 
+# Run the application
+
+#' Launch BayesPower Shiny App
+#'
+#' @export
+BayesPower_testing <- function(){
+
+pkgs <- c(
+  "rootSolve", "shiny", "gsl", "shinyWidgets", "shinyjs",
+  "kableExtra", "knitr", "Rcpp", "fontawesome", "BH",
+  "bslib", "pracma", "profvis", "mombf", "ExtDist",
+  "ggplot2", "patchwork"
+)
+
+invisible(lapply(pkgs, function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
+  library(pkg, character.only = TRUE)
+}))
+
+
+
+  # Run the application
+  shinyApp(ui = ui, server = server)
+
+}
+
+
 
 ui <- navbarPage(id = "id",
                  "\\(\\text{BayesPower}_{1.0}\\)",
