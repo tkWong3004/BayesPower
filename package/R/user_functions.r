8310955a898c68935f855fefd6f1654e3a7301ee
tkWong3004 <t.k.wong3004@gmail.com>
2025-08-27 13:05:30 +0200

    make the functions "smarter" and updated documentation

--- package/R/user_functions.r
+++ package/R/user_functions.r
@@ -26,7 +26,6 @@
 #' \dontrun{
 #' bp_t.test_one_sample(
 #'   hypothesis = "!=",
-#'   e = NULL,
 #'   interval = "1",
 #'   D = 3,
 #'   target = 0.8,
@@ -35,10 +34,6 @@
 #'   location = 0,
 #'   scale = 0.707,
 #'   dff = 1,
-#'   model_d = NULL,
-#'   location_d = NULL,
-#'   scale_d = NULL,
-#'   dff_d = NULL,
 #'   de_an_prior = 1,
 #'   N = NULL,
 #'   mode_bf = 1,
@@ -47,12 +42,12 @@
 #' }
 #'
 #' @export
-bp_t.test_one_sample <- function(hypothesis, e, interval,
-                                 D, target, alpha,
-                                 model, location, scale, dff,
-                                 model_d, location_d, scale_d, dff_d,
-                                 de_an_prior,
-                                 N, mode_bf, direct) {
+bp_t.test_one_sample <- function(hypothesis = NULL, e = NULL, interval = NULL,
+                                 D = NULL, target = NULL, alpha = NULL,
+                                 model = NULL, location = NULL, scale = NULL, dff = NULL,
+                                 model_d = NULL, location_d = NULL, scale_d = NULL, dff_d = NULL,
+                                 de_an_prior = NULL,
+                                 N = NULL, mode_bf = NULL, direct = NULL) {
 
   # Automatically assign 1 to any NULL argument
   args <- list(D = D, target = target, model = model, location = location, scale = scale,
@@ -82,13 +77,23 @@ bp_t.test_one_sample <- function(hypothesis, e, interval,
   e <- args$e
 
   # Call appropriate table function
-  if (interval == 1) {
-    t1_Table(D, target, model, location, scale, dff, hypothesis,
-             model_d, location_d, scale_d, dff_d, de_an_prior, N, mode_bf, alpha, direct)
-  } else {
-    t1e_table(D, target, model, scale, dff, hypothesis, e,
-              model_d, scale_d, dff_d, de_an_prior, N, mode_bf, location_d, alpha, direct)
-  }
+  # Call appropriate table function with error handling
+  tryCatch(
+    suppressWarnings({
+      if (interval == 1) {
+        t1_Table(D, target, model, location, scale, dff, hypothesis,
+                 model_d, location_d, scale_d, dff_d, de_an_prior, N, mode_bf, alpha, direct)
+      } else {
+        t1e_table(D, target, model, scale, dff, hypothesis, e,
+                  model_d, scale_d, dff_d, de_an_prior, N, mode_bf, location_d, alpha, direct)
+      }
+    }),
+    error = function(e) {
+      message("Sample size cannot be determined")
+      return(NULL)
+    }
+  )
+
 }
 
 #' Sample size determination for two-sample Bayesian t-test
@@ -131,13 +136,7 @@ bp_t.test_one_sample <- function(hypothesis, e, interval,
 #'   location = 0,
 #'   scale = 0.707,
 #'   dff = 1,
-#'   model_d = NULL,
-#'   location_d = NULL,
-#'   scale_d = NULL,
-#'   dff_d = NULL,
 #'   de_an_prior = 1,
-#'   N1 = NULL,
-#'   N2 = NULL,
 #'   r = 1,
 #'   mode_bf = 1,
 #'   direct = "h1"
@@ -145,21 +144,30 @@ bp_t.test_one_sample <- function(hypothesis, e, interval,
 #' }
 #'
 #' @export
-bp_t.test_two_sample <- function(hypothesis, e, interval,
-                                 D, target, alpha,
-                                 model, location, scale, dff,
-                                 model_d, location_d, scale_d, dff_d,
-                                 de_an_prior,
-                                 N1, N2, r, mode_bf, direct) {
+bp_t.test_two_sample <- function(hypothesis = NULL, e = NULL, interval = NULL,
+                                 D = NULL, target = NULL, alpha = NULL,
+                                 model = NULL, location = NULL, scale = NULL, dff = NULL,
+                                 model_d = NULL, location_d = NULL, scale_d = NULL, dff_d = NULL,
+                                 de_an_prior = NULL,
+                                 N1 = NULL, N2 = NULL, r = NULL, mode_bf = NULL, direct = NULL) {
 
-  if (interval == "1") {
-    t2_Table(D, r, target, model, location, scale, dff, hypothesis,
-             model_d, location_d, scale_d, dff_d, de_an_prior, N1, N2, mode_bf, alpha, direct)
-  } else {
-    t2e_table(D, r, target, model, scale, dff, hypothesis, e,
-              model_d, scale_d, dff_d, de_an_prior, mode_bf, location, N1, N2, alpha, direct)
-  }
+  tryCatch(
+    suppressWarnings({
+      if (!is.null(interval) && interval == "1") {
+        t2_Table(D, r, target, model, location, scale, dff, hypothesis,
+                 model_d, location_d, scale_d, dff_d, de_an_prior, N1, N2, mode_bf, alpha, direct)
+      } else {
+        t2e_table(D, r, target, model, scale, dff, hypothesis, e,
+                  model_d, scale_d, dff_d, de_an_prior, mode_bf, location, N1, N2, alpha, direct)
+      }
+    }),
+    error = function(e) {
+      message("Sample size cannot be determined")
+      return(NULL)
+    }
+  )
 }
+
 
 #' Sample size determination for Bayesian correlation test
 #'
@@ -202,43 +210,43 @@ bp_t.test_two_sample <- function(hypothesis, e, interval,
 #'   FP = 0.05,
 #'   model = "d_beta",
 #'   k = 1,
-#'   alpha = NULL,
-#'   beta = NULL,
-#'   scale = NULL
-#'   model_d = NULL,
-#'   alpha_d = NULL,
-#'   beta_d = NULL,
-#'   location_d = NULL,
-#'   k_d = NULL,
-#'   scale_d = NULL,
 #'   de_an_prior = 1,
-#'   N = NULL,
 #'   mode_bf = 1,
 #'   direct = "h1"
 #' )
 #' }
 #'
 #' @export
-bp_cor <- function(hypothesis, h0, e, interval,
-                   D, target, FP,
-                   model, k, alpha, beta, scale,
-                   model_d, alpha_d, beta_d, location_d, k_d, scale_d,
-                   de_an_prior,
-                   N, mode_bf, direct) {
+bp_cor <- function(hypothesis = NULL, h0 = NULL, e = NULL, interval = NULL,
+                   D = NULL, target = NULL, FP = NULL,
+                   model = NULL, k = NULL, alpha = NULL, beta = NULL, scale = NULL,
+                   model_d = NULL, alpha_d = NULL, beta_d = NULL, location_d = NULL,
+                   k_d = NULL, scale_d = NULL,
+                   de_an_prior = NULL,
+                   N = NULL, mode_bf = NULL, direct = NULL) {
 
-  location = h0
-  dff=dff_d=1
-  if (interval == "1") {
-    r_table(D, target, model, k, alpha, beta, h0, location, scale, dff,
-            hypothesis, model_d, location_d, k_d, alpha_d, beta_d, scale_d,
-            dff_d, de_an_prior, N, mode_bf, FP, direct)
+  location <- h0
+  dff <- dff_d <- 1
 
-  } else {
-    re_table(D, target, model, k, alpha, beta, h0, location, scale, dff,
-             hypothesis, model_d, location_d, k_d, alpha_d, beta_d, scale_d,
-             dff_d, de_an_prior, N, mode_bf, FP, e, direct)
-  }
+  tryCatch(
+    suppressWarnings({
+      if (!is.null(interval) && interval == "1") {
+        r_table(D, target, model, k, alpha, beta, h0, location, scale, dff,
+                hypothesis, model_d, location_d, k_d, alpha_d, beta_d, scale_d,
+                dff_d, de_an_prior, N, mode_bf, FP, direct)
+      } else {
+        re_table(D, target, model, k, alpha, beta, h0, location, scale, dff,
+                 hypothesis, model_d, location_d, k_d, alpha_d, beta_d, scale_d,
+                 dff_d, de_an_prior, N, mode_bf, FP, e, direct)
+      }
+    }),
+    error = function(e) {
+      message("Sample size cannot be determined")
+      return(NULL)
+    }
+  )
 }
+
 
 #' Sample size determination for Bayesian F-test
 #'
@@ -253,14 +261,14 @@ bp_cor <- function(hypothesis, h0, e, interval,
 #' @param FP The targeted false positive rate (if \code{direct = "h1"}) or false negative rate (if \code{direct = "h0"}).
 #' @param p Number of predictors in the reduced model.
 #' @param k Number of predictors in the full model.
-#' @param model Statistical model of the analysis prior under the alternative hypothesis.
-#' @param dff Degrees of freedom for the analysis prior under the alternative hypothesis.
-#' @param rscale Scaling parameter for the analysis prior.
-#' @param f_m Cohen's f² effect size for the analysis prior.
-#' @param model_d Statistical model of the design prior under the alternative hypothesis.
-#' @param dff_d Degrees of freedom for the design prior under the alternative hypothesis.
-#' @param rscale_d Scaling parameter for the design prior.
-#' @param f_m_d Cohen's f² effect size for the design prior.
+#' @param model Statistical model of the analysis prior under the alternative hypothesis: effect size prior  (\code{"effectsize"}) or Moment prior (\code{"Moment"})
+#' @param dff Degrees of freedom for the analysis prior under the alternative hypothesis.(must be >3 if moment prior is used)
+#' @param rscale Scaling parameter for the analysis effect size prior.
+#' @param f_m Cohen's f effect size parameter for the analysis prior.
+#' @param model_d Statistical model of the design prior under the alternative hypothesis:: effect size prior  (\code{"effectsize"}), Moment prior (\code{"Moment"}), or Point prior (\code{"Point"})
+#' @param dff_d Degrees of freedom for the design prior under the alternative hypothesis. (must be >3 if moment prior is used)
+#' @param rscale_d Scaling parameter for the design effect size prior.
+#' @param f_m_d Cohen's f effect size parameter for the design prior or the point design prior.
 #' @param de_an_prior Integer (0 or 1). If 1, analysis and design priors under the alternative are the same; if 0, they are not.
 #' @param N Sample size.
 #' @param mode_bf Integer (0 or 1). If \code{1}, sample size determination; if \code{2}, \code{N} is needed for the calculation of probabilities of compelling and misleading evidence.
@@ -280,35 +288,36 @@ bp_cor <- function(hypothesis, h0, e, interval,
 #'   dff = 1,
 #'   rscale = 0.5,
 #'   f_m = 0.15,
-#'   model_d = NULL,
-#'   dff_d = NULL,
-#'   rscale_d = NULL,
-#'   f_m_d = NULL,
 #'   de_an_prior = 1,
-#'   N = NULL,
 #'   mode_bf = 1,
 #'   direct = "h1"
 #' )
 #' }
 #'
 #' @export
-bp_f <- function(interval,
-                 D, target, FP, p, k,
-                 model, dff, rscale, f_m,
-                 model_d, dff_d, rscale_d, f_m_d,
-                 de_an_prior,
-                 N, mode_bf, direct) {
+bp_f <- function(interval = NULL,
+                 D = NULL, target = NULL, FP = NULL, p = NULL, k = NULL,
+                 model = NULL, dff = NULL, rscale = NULL, f_m = NULL,
+                 model_d = NULL, dff_d = NULL, rscale_d = NULL, f_m_d = NULL,
+                 de_an_prior = NULL,
+                 N = NULL, mode_bf = NULL, direct = NULL, e = NULL) {
 
-  if (interval == "1") {
-    f_table(D, target, p, k, dff, rscale, f_m, model,
-            dff_d, rscale_d, f_m_d, model_d, de_an_prior, N,
-            mode_bf, FP, direct)
-
-  } else {
-    fe_table(D, target, p, k, dff, rscale, f_m, model,
-             dff_d, rscale_d, f_m_d, model_d, de_an_prior, N,
-             mode_bf, e, FP, direct)
-  }
+  tryCatch({
+    suppressWarnings({
+      if (interval == "1") {
+        f_table(D, target, p, k, dff, rscale, f_m, model,
+                dff_d, rscale_d, f_m_d, model_d, de_an_prior, N,
+                mode_bf, FP, direct)
+      } else {
+        fe_table(D, target, p, k, dff, rscale, f_m, model,
+                 dff_d, rscale_d, f_m_d, model_d, de_an_prior, N,
+                 mode_bf, e, FP, direct)
+      }
+    })
+  }, error = function(e) {
+    message("Sample size cannot be determined")
+    return(invisible(NULL))
+  })
 }
 
 
@@ -324,14 +333,14 @@ bp_f <- function(interval,
 #' @param target The targeted true positive rate (if \code{direct = "h1"}) or true negative rate (if \code{direct = "h0"}).
 #' @param FP The targeted false positive rate (if \code{direct = "h1"}) or false negative rate (if \code{direct = "h0"}).
 #' @param location Null proportion value.
-#' @param model Statistical model of the analysis prior under the alternative hypothesis.
+#' @param model Statistical model of the analysis prior under the alternative hypothesis: beta prior  (\code{"beta"}) or Moment prior (\code{"Moment"})
 #' @param alpha Parameter for the analysis prior under the alternative hypothesis.
 #' @param beta Parameter for the analysis prior under the alternative hypothesis.
 #' @param scale Scale parameter for the analysis prior under the alternative hypothesis.
-#' @param model_d Statistical model of the design prior under the alternative hypothesis.
+#' @param model_d Statistical model of the design prior under the alternative hypothesis:beta prior  (\code{"beta"}) , Moment prior (\code{"Moment"}), or Point prior (\code{"Point"})
 #' @param alpha_d Parameter for the design prior under the alternative hypothesis.
 #' @param beta_d Parameter for the design prior under the alternative hypothesis.
-#' @param location_d Null proportion value for the design prior.
+#' @param location_d The proportion value for the design point prior.
 #' @param scale_d Scale parameter for the design prior under the alternative hypothesis.
 #' @param de_an_prior Integer (0 or 1). If 1, analysis and design priors under the alternative are the same; if 0, they are not.
 #' @param N Sample size.
@@ -351,40 +360,37 @@ bp_f <- function(interval,
 #'   model = "beta",
 #'   alpha = 1,
 #'   beta = 1,
-#'   scale = NULL,
-#'   model_d = NULL,
-#'   alpha_d = NULL,
-#'   beta_d = NULL,
-#'   location_d = NULL,
-#'   scale_d = NULL,
 #'   de_an_prior = 1,
-#'   N = NULL,
 #'   mode_bf = 1,
-#'   e = NULL,
 #'   direct = "h1"
 #' )
 #' }
 #'
 #' @export
-bp_bin <- function(hypothesis, interval,
-                   D, target, FP, location,
-                   model, alpha, beta, scale,
-                   model_d, alpha_d, beta_d, location_d, scale_d,
-                   de_an_prior,
-                   N, mode_bf, e = NULL, direct) {
+bp_bin <- function(hypothesis = NULL, interval = NULL,
+                   D = NULL, target = NULL, FP = NULL, location = NULL,
+                   model = NULL, alpha = NULL, beta = NULL, scale = NULL,
+                   model_d = NULL, alpha_d = NULL, beta_d = NULL, location_d = NULL, scale_d = NULL,
+                   de_an_prior = NULL,
+                   N = NULL, mode_bf = NULL, e = NULL, direct = NULL) {
 
-  if (interval == "1") {
-    bin_table(D, target, alpha, beta, location, scale, model, hypothesis,
-              alpha_d, beta_d, location_d, scale_d, model_d, de_an_prior, N,
-              mode_bf, FP, direct)
-
-  } else {
-    bin_e_table(D, target, alpha, beta, location, scale, model, hypothesis,
-                alpha_d, beta_d, location_d, scale_d, model_d, de_an_prior, N,
-                mode_bf, FP, e, direct)
-  }
+  tryCatch({
+    suppressWarnings({
+      if (!is.null(interval) && interval == "1") {
+        bin_table(D, target, alpha, beta, location, scale, model, hypothesis,
+                  alpha_d, beta_d, location_d, scale_d, model_d, de_an_prior, N,
+                  mode_bf, FP, direct)
+      } else {
+        bin_e_table(D, target, alpha, beta, location, scale, model, hypothesis,
+                    alpha_d, beta_d, location_d, scale_d, model_d, de_an_prior, N,
+                    mode_bf, FP, e, direct)
+      }
+    })
+  }, error = function(e) {
+    message("Sample size cannot be determined")
+    return(invisible(NULL))
+  })
 }
-
 
 #' Sample size determination for Bayesian test of two proportions
 #'
@@ -393,14 +399,14 @@ bp_bin <- function(hypothesis, interval,
 #'
 #' @param D The bound of compelling evidence.
 #' @param target The targeted true positive rate (if \code{direct = "h1"}) or true negative rate (if \code{direct = "h0"}).
-#' @param a0 Alpha parameter of the null distribution (group proportion under the null).
-#' @param b0 Beta parameter of the null distribution (group proportion under the null).
-#' @param model1 Statistical model of the analysis prior for group 1.
-#' @param a1 Alpha parameter for group 1 under the alternative hypothesis.
-#' @param b1 Beta parameter for group 1 under the alternative hypothesis.
-#' @param a2 Alpha parameter for group 2 under the alternative hypothesis.
-#' @param b2 Beta parameter for group 2 under the alternative hypothesis.
-#' @param model2 Statistical model of the analysis prior for group 2.
+#' @param a0 Alpha parameter of the beta distribution under the null .
+#' @param b0 Beta parameter of the beta distribution  under the null.
+#' @param model1 Statistical model of the design prior for group 1: beta (\code{"beta"}), Point prior (\code{"Point"}, or same as analysis prior \code{"same"})
+#' @param a1 Alpha parameter of the analysis beta prior  distribution for group 1 under the alternative hypothesis.
+#' @param b1 Beta parameter of the analysis beta prior  distribution for group 1 under the alternative hypothesis.
+#' @param a2 Alpha parameter of the analysis beta prior distribution for group 2 under the alternative hypothesis.
+#' @param b2 Beta parameter  of the analysis beta prior  distribution for group 2 under the alternative hypothesis.
+#' @param model2 Statistical model of the design prior for group 1: beta (\code{"beta"}), or Point prior (\code{"Point"}, or same as analysis prior \code{"same"})
 #' @param a1d Alpha parameter for the design prior of group 1.
 #' @param b1d Beta parameter for the design prior of group 1.
 #' @param dp1 True proportion for group 1 in the design prior.
@@ -425,26 +431,26 @@ bp_bin <- function(hypothesis, interval,
 #'   a2 = 1,
 #'   b2 = 1,
 #'   model2 = "same",
-#'   a1d = NULL,
-#'   b1d = NULL,
-#'   dp1 = NULL,
-#'   a2d = NULL,
-#'   b2d = NULL,
-#'   dp2 = NULL,
 #'   mode_bf = 1,
-#'   n1 = NULL,
-#'   n2 = NULL,
 #'   direct = "h1"
 #' )
 #' }
 #'
 #' @export
-bp_props <- function(D, target, a0, b0, a1, b1, a2, b2, model1,
-                     a1d, b1d, dp1, model2, a2d, b2d, dp2, mode_bf, n1, n2, direct) {
+bp_props <- function(D = NULL, target = NULL, a0 = NULL, b0 = NULL, a1 = NULL, b1 = NULL,
+                     a2 = NULL, b2 = NULL, model1 = NULL,
+                     a1d = NULL, b1d = NULL, dp1 = NULL, model2 = NULL,
+                     a2d = NULL, b2d = NULL, dp2 = NULL,
+                     mode_bf = NULL, n1 = NULL, n2 = NULL, direct = NULL) {
 
-  r = 1
-  pro_table_p2(D, target, a0, b0, a1, b1, a2, b2, r, model1,
-               a1d, b1d, dp1, model2, a2d, b2d, dp2, mode_bf, n1, n2, direct)
+  r <- 1
+  tryCatch({
+    suppressWarnings({
+      pro_table_p2(D, target, a0, b0, a1, b1, a2, b2, r, model1,
+                   a1d, b1d, dp1, model2, a2d, b2d, dp2, mode_bf, n1, n2, direct)
+    })
+  }, error = function(e) {
+    message("Sample size cannot be determined")
+    return(invisible(NULL))
+  })
 }
-
-
