f460ca887c81fa670487163d0720931758a232d7
tkWong3004 <t.k.wong3004@gmail.com>
2026-03-16 11:32:54 +0100

    Fixing the supporting r scripts

--- BF_absolute_error/analysis.r
+++ BF_absolute_error/analysis.r
@@ -12,83 +12,83 @@ source("functions.r")
 # absolute errors are shown.
 #### t-test ####
 # specification of analysis prior and sample size
-df=49
-location=0
-scale=1
-dff=1
-t.cri=seq(0.1,5,.01)
-prior_analysis=c("t-distribution","Normal","Moment")
-alternative ="two.sided"
-n1=50
-r=1
+df             <- 49
+location       <- 0
+scale          <- 1
+dff            <- 1
+t.cri          <- seq(0.1,5,.01)
+prior_analysis <- c("t-distribution","Normal","Moment")
+alternative    <- "two.sided"
+n1             <- 50
+r              <- 1
 
-abs_error_t1 = array(NA, dim=c(3,length(t.cri)))
-abs_error_t2 = array(NA, dim=c(3,length(t.cri)))
+abs_error_t1 <- array(NA, dim=c(3,length(t.cri)))
+abs_error_t2 <- array(NA, dim=c(3,length(t.cri)))
 
 for( ii in 1:length(prior_analysis)){
 for( i in 1:length(t.cri)){
-  t=c(-t.cri[i],t.cri[i]) # specifying which critical value is used for power calculation
-  abs_error_t1[ii,i]=t1_TPE_abs_error(t, df, prior_analysis[ii], location, scale, dff)
-  abs_error_t2[ii,i]=t2_TPE_abs_error(t,n1,r,prior_analysis[ii] ,location ,scale,dff , alternative )
+  t <- c(-t.cri[i],t.cri[i]) # specifying which critical value is used for power calculation
+  abs_error_t1[ii,i] <- t1_TPE_abs_error(t, df, prior_analysis[ii], location, scale, dff)
+  abs_error_t2[ii,i] <- t2_TPE_abs_error(t,n1,r,prior_analysis[ii] ,location ,scale,dff , alternative )
 }}
 max(abs_error_t1,abs_error_t2)
 #### correlation ####
 #specification of analysis prior and sample size
-r.cri=seq(0.01,.5,.01)
-n=50
-k=1
-alpha=beta=2
-h0=location=0
-scale=.01
-dff=1
-prior_analysis = c("d_beta","beta","Moment")
+r.cri          <- seq(0.01,.5,.01)
+n              <- 50
+k              <- 1
+alpha          <- beta     <- 2
+h0             <- location <- 0
+scale          <- .01
+dff            <- 1
+prior_analysis <-  c("d_beta","beta","Moment")
 
-abs_error_r = array(NA, dim=c(3,length(r.cri)))
+abs_error_r    <- array(NA, dim=c(3,length(r.cri)))
 
 for( ii in 1:length(prior_analysis)){
   for( i in 1:length(r.cri)){
-    r=c(-r.cri[i],r.cri[i]) # specifying which critical value is used for power calculation
-    abs_error_r[ii,i]=r_TPE_abs_error(r,n,k, alpha, beta,h0,alternative,location,scale,dff,prior_analysis[ii])
+    r <- c(-r.cri[i],r.cri[i]) # specifying which critical value is used for power calculation
+    abs_error_r[ii,i] <- r_TPE_abs_error(r,n,k, alpha, beta,h0,alternative,location,scale,dff,prior_analysis[ii])
   }}
 max(abs_error_r)
 #### f-test ####
 # specification of analysis prior, sample size and the number
 # of predictors in the reduced model (p) and full model (k).
 
-f.cri = seq(0.1,5,.01)
-k=10
-p=2
-q= k-p
-n=50
-m= n-p
-prior_analysis = c("effectsize", "Moment")
-dff=3
-rscale=.1
-f_m=sqrt(.1)
-abs_error_f = array(NA, dim=c(2,length(f.cri)))
+f.cri          <- seq(0.1,5,.01)
+k              <- 10
+p              <- 2
+q              <- k-p
+n              <- 50
+m              <- n-p
+prior_analysis <- c("effectsize", "Moment")
+dff            <- 3
+rscale         <- .1
+f_m            <- sqrt(.1)
+abs_error_f    <- array(NA, dim=c(2,length(f.cri)))
 for( ii in 1:length(prior_analysis)){
   for( i in 1:length(f.cri)){
-    f=f.cri[i] # specifying which critical value is used for power calculation
-    abs_error_f[ii,i]=F_TPE_abs_error(f,q,m,dff,rscale,f_m,prior_analysis[ii])
+    f <- f.cri[i] # specifying which critical value is used for power calculation
+    abs_error_f[ii,i] <- F_TPE_abs_error(f,q,m,dff,rscale,f_m,prior_analysis[ii])
 
   }}
 max(abs_error_f)
 #### one-proportion ####
 # specification of analysis prior and sample size
-n=50
-h0=location=.5
-alpha=beta=1
-scale=.1
-alternative = "two.sided"
-prior_analysis = c("beta","Moment")
-abs_error_bin = array(NA, dim=c(2,25))
+n               <- 50
+h0              <- location  <-.5
+alpha           <- beta      <- 1
+scale           <- .1
+alternative     <- "two.sided"
+prior_analysis  <- c("beta","Moment")
+abs_error_bin   <- array(NA, dim=c(2,25))
 
-lower= 24:0
-upper = 26:50
+lower          <-  24:0
+upper          <-  26:50
 for( ii in 1:length(prior_analysis)){
   for( i in 1:(25)){
-    x=c(lower[i],upper[i]) # specifying which critical value is used for power calculation
-    abs_error_bin[ii,i]=bin_TPE_abs_error(x,n,h0,alpha,beta,location,scale,prior_analysis[ii],alternative)
+    x <- c(lower[i],upper[i]) # specifying which critical value is used for power calculation
+    abs_error_bin[ii,i] <- bin_TPE_abs_error(x,n,h0,alpha,beta,location,scale,prior_analysis[ii],alternative)
 
 
   }}
